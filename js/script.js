f90b5b2d057ebc1fc97c9a790c2acbe79e9f6f59
deadly8 <tarasent@ukr.net>
2017-05-05 13:46:34 +0300

    added timepicker

--- js/script.js
+++ js/script.js
@@ -1,40 +1,65 @@
-/*
- * Mobile menu
- */
-var $menu = $("#mobile-menu").mmenu({
+$( document ).ready(function() {
+    /*
+     * Mobile menu
+     */
+    var $menu = $("#mobile-menu").mmenu({
 
 
-        "navbar": {
-            title: ""
+            "navbar": {
+                title: ""
+            },
+            "navbars": [
+
+            ]
         },
-        "navbars": [
+        {
 
-        ]
-    },
-    {
+            "searchfield": {
+                "clear": true
+            }
 
-        "searchfield": {
-            "clear": true
-        }
+        });
 
+    var $icon = $("#open-menu");
+    var API = $menu.data( "mmenu" );
+    $icon.on( "click", function() {
+        API.open();
     });
 
-var $icon = $("#open-menu");
-var API = $menu.data( "mmenu" );
-$icon.on( "click", function() {
-    API.open();
+    $("#datetimepicker1").datetimepicker({
+        format: 'DD/MM/YYYY',
+        widgetPositioning: {
+            horizontal: 'left'
+
+        }
+
+
+    });
+    $("#datetimepicker2").datetimepicker({
+        useCurrent: false, //Important! See issue #1075
+        format: 'DD/MM/YYYY'
+    });
+    $("#datetimepicker1").on("dp.change", function (e) {
+        $('#datetimepicker2').data("DateTimePicker").minDate(e.date);
+    });
+    $("#datetimepicker2").on("dp.change", function (e) {
+        $('#datetimepicker1').data("DateTimePicker").maxDate(e.date);
+    });
 });
 
+
+
+
 /*var $iconClose = $("#close-menu");
-$iconClose.on( "click", function() {
-    API.close();
-});*/
+ $iconClose.on( "click", function() {
+ API.close();
+ });*/
 
 /*$(window).on("resize", function() {
-    if (window.matchMedia("(min-width: 992px)").matches) {
-        API.close();
-    }
-});*/
+ if (window.matchMedia("(min-width: 992px)").matches) {
+ API.close();
+ }
+ });*/
 
 /*
  * end mobile mmenu
